5733ea25d3c146203c03950977ffc6a4c998b1e3
liziyue17 <liziyue17@mails.tsinghua.edu.cn>
2020-04-12 14:44:01 +0800

    Finish! Submit All Files!
    Congratulations!

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,9 @@ import pygame
 
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -12,21 +15,31 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
+    #创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    #创建一个用于存储游戏统计信息的实例，并创建计分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    #创建一艘飞船,一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-    #创建一个用于储存子弹的编组
     bullets = Group()
+    aliens = Group()
 
-    #设置背景色
-    bg_color = (230, 230, 230)
-
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 
